460f2183b9f1f5c370e345e0c7226802497eec30
Tomislav Capan <tomislav.capan@gmail.com>
2018-04-08 17:46:25 +0200

    DB backup script tweak: full path to AWS CLI executable

--- express/notejam/cron/db-backup.sh
+++ express/notejam/cron/db-backup.sh
@@ -14,7 +14,7 @@ BACKUP_LOCAL_PATH=/tmp/${BACKUP_FILENAME}
 BACKUP_S3_PATH=s3://topal.devops.notejam.backup/${ENV}/${BACKUP_FILENAME}
 
 tar -czf $BACKUP_LOCAL_PATH -C $APP_DIR $DB_FILE
-aws s3 cp $BACKUP_LOCAL_PATH $BACKUP_S3_PATH --quiet
+/usr/local/bin/aws s3 cp $BACKUP_LOCAL_PATH $BACKUP_S3_PATH --quiet
 rm ${BACKUP_LOCAL_PATH}
 
 exit 0
